e6309c68bb534f0b0be018a66859daaccd73c371
NGUYENMINHTRONG <77635440+nguyenminhtrong1@users.noreply.github.com>
2021-08-24 09:34:56 +0700

    kiem tra dang nhap

--- DaySoForm/Program.cs
+++ DaySoForm/Program.cs
@@ -10,7 +10,28 @@ namespace DaySoForm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("helo");
+            string pass;
+            int count = 0;
+            Console.Write("Mat khau mac dinh la: bai1\n");
+            Console.Write("---------------------------------\n");
+
+            do
+            {
+                Console.Write("Mat khau: ");
+                pass = Console.ReadLine();
+
+                count++;
+
+            }
+            while ((pass != "bai1") && (count != 3));
+
+            if (count == 3)
+                Console.Write("Dang nhap that bai!");
+            else
+                Console.Write("ok");
+
+            Console.ReadKey();
+
         }
     }
 }
